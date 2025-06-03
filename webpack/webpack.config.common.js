871a5c2618dab05dfdf2216567966de994948d07
Zach <zach000317@gmail.com>
2025-06-03 16:47:55 +0800

    refactor: 引入路径使用别名

--- webpack/webpack.config.common.js
+++ webpack/webpack.config.common.js
@@ -1,37 +1,41 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 // 相对路径转绝对路径
-const pathResolve = (_path) => path.resolve(__dirname, _path)
+const pathResolve = (_path) => path.resolve(__dirname, _path);
 
 module.exports = {
-  entry: pathResolve('../src/index.jsx'),
+  entry: pathResolve("../src/index.jsx"),
   output: {
-    path:pathResolve('../dist'),
-    filename: 'scripts/[name].[hash].bundle.js',
+    path: pathResolve("../dist"),
+    filename: "scripts/[name].[hash].bundle.js",
     clean: true,
   },
-  module:{
-    rules:[
+  module: {
+    rules: [
       {
         test: /\.jsx$/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-react', '@babel/preset-env']
-          }
-        }
-      }
-    ]
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+          },
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: pathResolve('../public/index.html'),
-      filename: 'index.html',
-      title: 'zach',
-    })
+      template: pathResolve("../public/index.html"),
+      filename: "index.html",
+      title: "zach",
+    }),
   ],
   resolve: {
-    extensions: ['.js', '.jsx', '.json'],
-  }
-}
+    extensions: [".js", ".jsx", ".json"],
+    alias: {
+      "@": pathResolve("../src/components"),
+      "#": pathResolve("../src/common"),
+    },
+  },
+};
--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -1,27 +1,29 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
-  module:{
-    rules:[
+  module: {
+    rules: [
       {
-        test:/\.css$/,
-        use:['style-loader','css-loader','postcss-loader']
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "postcss-loader"],
       },
       {
-        test:/\.less$/,
-        use:['style-loader','css-loader','postcss-loader','less-loader']
-      }
-    ]
+        test: /\.less$/,
+        use: ["style-loader", "css-loader", "postcss-loader", "less-loader"],
+      },
+    ],
   },
-  devServer:{
-    host:'127.0.0.1',
-    port:9000,
-    open:true,
-    hot:true,
-  }
-}
+  devServer: {
+    host: "127.0.0.1",
+    port: 9000,
+    open: true,
+    hot: true,
+    // react配置路由之后，直接输入url显示404,是因为少了下面的配置
+    historyApiFallback: true,
+  },
+};
